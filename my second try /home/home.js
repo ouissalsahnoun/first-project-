a605c59308f6beb6c12e609bf883b118db3deddf
“ouissalsahnoun” <ajuraiss@gmail.com>
2021-07-07 03:00:04 +0300

    second try

---
+++ my second try /home/home.js
@@ -0,0 +1,62 @@
+$("#visible").click(function(){
+    $("#invisible").toggleClass("show")
+    
+    })
+
+    $("#txt2").after("<div class='all'> </div>")
+
+    $(".all").append("<div id='div1'> Name:Sara ,Category :College,story: College Experience The college experience is my initial step into finding my future career. Leaving home  for the first time in my life and finding out if I... </div>"+
+    
+    "<div id='di21'>Name:anonymous , Category: child abuse , Story:  Well where do I start? I was born in Christchurch, New Zealand. I’m 23 years years of age and this is my story. When I was five, my family left me and I... </div>"+
+    
+    
+    "<div id='div3'> Name:Ahmed, category:new house, Story: I have been looking forward to sharing this post with you because it’s been so many of you have been following our journey since I shared that we had put our hous... </div>")
+    
+
+
+
+
+
+
+ $("#home").click(function(){
+
+    window.location.href = 'home.html'});
+
+  
+
+    $("#read").click(function(){
+
+      window.location.href = 'read.html'});
+
+
+
+
+
+ $("#shareus").click(function(){
+
+    window.location.href = 'write.html'});
+
+ $("#home").click(function(){
+
+    window.location.href = 'home.html'});
+
+
+ $("#aboutus").click(function(){
+
+    window.location.href = 'about.html'});
+
+ $("#contactus").click(function(){
+
+    window.location.href = 'contact.html'});
+
+
+
+    $(".all").click(function(){
+
+
+      window.location.href = 'read.html'
+
+
+    })
+
+   
